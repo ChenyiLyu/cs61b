e97e1a34b6568fc1102f16e417a4d205acd03c52
Chenyi Lyu <clyu4@jhu.edu>
2021-02-27 00:27:31 -0500

    proj1gold first try

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,30 +8,21 @@ public class TestArrayDequeGold {
 
     /** @rule Test should randomly call StudentArrayDeque and ArrayDequeSolution.*/
     @Test
-    public void testAddRemove() {
-        StudentArrayDeque<Integer> sAD = new StudentArrayDeque<>();
+    public void testAddRemove() {    StudentArrayDeque<Integer> sAD = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> mAD = new ArrayDequeSolution<>();
-        for (int i = 0; i < 20; i++) {
-
-            double seed = StdRandom.uniform();
-            if (seed < 0.25 | (sAD.isEmpty() && mAD.isEmpty())) {
-                System.out.println('1');
-                sAD.addFirst(i); mAD.addFirst(i);
-            } else if (seed < 0.5) {
-                System.out.println('2');
-                sAD.addLast(i); mAD.addLast(i);
-            } else if (seed < 0.75) {
-                System.out.println('3');
-                Integer s = sAD.removeFirst();
-                Integer m = mAD.removeFirst();
-                assertEquals(m, s);
-            } else {
-                System.out.println('4');
-                Integer s = sAD.removeLast();
-                Integer m = mAD.removeLast();
-                assertEquals(m, s);
-            }
-        }
+        int i = 3;
+        sAD.addFirst(i); mAD.addFirst(i);
+        sAD.addFirst(i); mAD.addFirst(i);
+        Integer s1 = sAD.removeFirst(); Integer m1 = mAD.removeFirst();
+        sAD.addFirst(i); mAD.addFirst(i);
+        sAD.addLast(i); mAD.addLast(i);
+        sAD.addLast(i); mAD.addLast(i);
+        sAD.addFirst(i); mAD.addFirst(i);
+        sAD.addFirst(i); mAD.addFirst(i);
+        sAD.addLast(i); mAD.addLast(i);
+        Integer s2 = sAD.removeLast();Integer m2 = mAD.removeLast();
+        Integer s3 = sAD.removeLast();Integer m3 = mAD.removeLast();
+        assertEquals(m3, s3);
     }
 
 
