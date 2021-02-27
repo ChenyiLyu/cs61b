50181893bed2f6516b60a7e9b1373ecbd6dd9acb
Chenyi Lyu <clyu4@jhu.edu>
2021-02-27 11:32:03 -0500

    second try!

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,28 +5,35 @@ import org.junit.Test;
  *  and removeLast methods. */
 
 public class TestArrayDequeGold {
-
+    private String message = "\n";
+    private String operation;
+    private static int randomTestRounds = 1000;
     /** @rule Test should randomly call StudentArrayDeque and ArrayDequeSolution.*/
     @Test
-    public void testAddRemove() {    StudentArrayDeque<Integer> sAD = new StudentArrayDeque<>();
+    public void testAddRemove() {
+        StudentArrayDeque<Integer> sAD = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> mAD = new ArrayDequeSolution<>();
-        int i = 3;
-        sAD.addFirst(i); mAD.addFirst(i);
-        sAD.addFirst(i); mAD.addFirst(i);
-        Integer s1 = sAD.removeFirst(); Integer m1 = mAD.removeFirst();
-        sAD.addFirst(i); mAD.addFirst(i);
-        sAD.addLast(i); mAD.addLast(i);
-        sAD.addLast(i); mAD.addLast(i);
-        sAD.addFirst(i); mAD.addFirst(i);
-        sAD.addFirst(i); mAD.addFirst(i);
-        sAD.addLast(i); mAD.addLast(i);
-        Integer s2 = sAD.removeLast();Integer m2 = mAD.removeLast();
-        Integer s3 = sAD.removeLast();Integer m3 = mAD.removeLast();
-        assertEquals(m3, s3);
+        for (int i = 0; i < randomTestRounds; i++) {
+            double seed = StdRandom.uniform();
+            if (seed < 0.25 | sAD.isEmpty() | sAD.isEmpty()) {
+                operation = "addFirst("+ i +")";
+                sAD.addFirst(i); mAD.addFirst(i);
+            } else if (seed < 0.5) {
+                operation = "addLast("+ i +")";
+                sAD.addLast(i); mAD.addLast(i);
+            } else if (seed < 0.75) {
+                Integer s = sAD.removeFirst(); Integer m = mAD.removeFirst();
+                assertEquals(message + "removeFirst(): "+ i, m, s);
+                operation = "removeFirst(): "+ i);
+            } else {
+                Integer s = sAD.removeLast(); Integer m = mAD.removeLast();
+                assertEquals(message + "removeLast(): "+ i, m, s);
+                operation = "removeLast(): "+ i);
+            }
+            message += operation + "\n";
+
+        }
     }
-
-
-
 
 
 }
